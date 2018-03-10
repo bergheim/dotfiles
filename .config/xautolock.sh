05d8469c789c9ed41137cb8c8ba3268bf2aea3ec
Thomas Bergheim <tbergheim@gmail.com>
2018-03-10 09:26:58 +0100

    xautolock: don't fork i3lock (fixes relocking). Set timeout to 5 min

--- .config/xautolock.sh
+++ .config/xautolock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 exec xautolock -detectsleep \
-  -time 3 -locker "$HOME/local/bin/fancylocki3" \
+  -time 5 -locker "$HOME/local/bin/fancylocki3" \
   -notify 30 \
   -notifier "notify-send -t 29000 -- 'LOCKING screen in 30 seconds'"
