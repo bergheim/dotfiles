18bb052d119329a0e33c9d59404d2f0e146e1bb9
Thomas Bergheim <thomas.bergheim@neptune-software.com>
2018-03-14 19:38:35 +0100

    xautolock: support suspension of autolocking
    Sometimes in a conference meeting this gets really annoying.
    Added a notice function so other processes can listen to the state. It could be
    used for other things than just xautolock, though.

--- .config/xautolock.sh
+++ .config/xautolock.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-exec xautolock -detectsleep \
-  -time 5 -locker "$HOME/local/bin/fancylocki3" \
-  -notify 30 \
-  -notifier "notify-send -t 29000 -- 'LOCKING screen in 30 seconds'"
+start() {
+  exec xautolock -detectsleep \
+     -time 5 -locker "$HOME/local/bin/fancylocki3" \
+     -notify 30 \
+     -notifier "notify-send -u low -- 'LOCKING screen in 30 seconds'"
+}
+
+autolock_pid=`pidof xautolock`
+if [ "$1" == "toggle" ]; then
+  if [ -z "$autolock_pid" ]; then
+    echo "start $autolock_pid"
+    exec notify-send -u low -- "Starting xautolock" &
+    notice clear
+    exec xautolock -detectsleep \
+         -time 5 -locker "$HOME/local/bin/fancylocki3" \
+         -notify 30 \
+         -notifier "notify-send -u low -- 'LOCKING screen in 30 seconds'" &
+  else
+    echo "stop $autolock_pid"
+    notice "autolock disabled"
+    exec notify-send -u low -- "Disabling xautolock" &
+    exec kill "$autolock_pid"
+  fi
+elif [ -z "$autolock_pid" ]; then
+  echo "Starting xautolock"
+  start
+fi
