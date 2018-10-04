703fe6f67e2e2fb4dac2e9187025777ff400ff96
Thomas Bergheim <thomas.bergheim@neptune-software.com>
2018-10-04 15:57:36 +0200

    qutebrowser: fix scrolling.bar after 1.5 upgrade

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -181,7 +181,7 @@ c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/2
 c.content.cookies.store = False
 
 c.scrolling.smooth = False
-c.scrolling.bar = True
+c.scrolling.bar = "always"
 
 c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}",
