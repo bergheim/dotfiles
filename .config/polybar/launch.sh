da6af3e011478b7fe335d2555dc9bf7175b8fc43
Thomas Bergheim <tbergheim@gmail.com>
2020-04-10 15:00:03 +0200

    polybar: add second bar for MONITOR_SECONDARY

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+xrdb ~/.Xresources
+
 polybar tb &
+polybar tb2 &
 
 echo "Bars launched..."
