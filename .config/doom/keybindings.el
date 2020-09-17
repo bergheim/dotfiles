44aff25ff50f589f078ab1cf9ef2b01f99c283e2
Thomas Bergheim <tbergheim@gmail.com>
2020-09-17 11:55:59 +0200

    doom: add some keybindings and packages

--- .config/doom/keybindings.el
+++ .config/doom/keybindings.el
@@ -1,29 +1,48 @@
 ;;; ~/.config/doom/keybindings.el -*- lexical-binding: t; -*-
 
+(map!
+ (:leader
 
-(map! :leader
-      ;; things I missed from spacemacs
-      "1" 'winum-select-window-1
-      "2" 'winum-select-window-2
-      "3" 'winum-select-window-3
-      "4" 'winum-select-window-4
-      ";" 'evilnc-comment-operator ;; was (pp-eval-expression EXPRESSION)
+   (:prefix ("a" . "custom bindings")
 
-      "agy" 'bergheim-toggle-yadm
-      "agd" 'magit-diff-buffer-file
-      "agl" 'magit-log-buffer-file
+     (:prefix ("g" . "git")
+       "y" 'bergheim-toggle-yadm
+       "d" 'magit-diff-buffer-file
+       "l" 'magit-log-buffer-file)
 
-      "ai" (lambda () (interactive) (find-file "~/org/inbox.org"))
-      "acg" 'org-clock-goto
-      "aci" 'org-clock-in
-      "acl" 'org-clock-in-last
-      "aco" 'org-clock-out
-      "acr" #'org-mru-clock-in
-      "acR" #'org-mru-clock-select-recent-task
-      "aL" 'org-store-link
-      "al" 'org-insert-link
+     :desc "org inbox" "i" (lambda () (interactive) (find-file "~/org/inbox.org"))
 
-      "am" 'mu4e
+     (:prefix ("o" . "org")
+       "a" 'org-agenda
+       "g" 'org-clock-goto
+       "i" 'org-clock-in
+       "l" 'org-clock-in-last
+       "o" 'org-clock-out
+       "c" 'org-capture
+       "r" #'org-mru-clock-in
+       "R" #'org-mru-clock-select-recent-task
+       "s" 'org-store-link
+       "l" 'org-insert-link)
 
-      "at" 'heaven-and-hell-toggle-theme
-      "aT" 'heaven-and-hell-load-default-theme)
+
+     ;; shortcuts for org since I use these so much
+     (:prefix ("c" . "org-clock")
+       "g" 'org-clock-goto
+       "i" 'org-clock-in
+       "l" 'org-clock-in-last
+       "o" 'org-clock-out
+       "c" 'org-capture
+       "r" #'org-mru-clock-in
+       "R" #'org-mru-clock-select-recent-task)
+
+     (:prefix ("d" . "dotfiles")
+       "d" 'find-in-dotfiles
+       "f" 'browse-dotfiles)
+
+     "m" 'mu4e
+     "e" 'elfeed
+
+     "r" 'rainbow-mode
+
+     "t" 'heaven-and-hell-toggle-theme
+     "T" 'heaven-and-hell-load-default-theme)))
