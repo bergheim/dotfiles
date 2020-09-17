9b832af2eefd39a8addbf359ea019df03be759dd
Thomas Bergheim <tbergheim@gmail.com>
2020-09-18 00:20:21 +0200

    doom: set relative line numbers
    Can't believe I've never really used it before..

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -35,7 +35,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
