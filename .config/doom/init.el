617dbd00e65ad3be2fcc7f58b985f27957dfd343
Thomas Bergheim <tbergheim@gmail.com>
2021-09-30 12:50:13 +0200

    doom: format on save and add lsp to web mode

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -58,7 +58,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format -onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -175,7 +175,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       (web +lsp)          ; the tubes
        yaml              ; JSON, but readable
 
        :email
