1fc8dac71c9da968bbe25352062c4701cc52f413
Thomas Bergheim <tbergheim@gmail.com>
2021-12-28 17:23:22 +0100

    org: customize a bunch of variables..

--- .config/doom/+org.el
+++ .config/doom/+org.el
@@ -5,4 +5,49 @@
 
 ;; (setq org-directory "~/org/")
 
-(setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
+(setq org-deadline-warning-days 14
+      ;; show tasks scheduled or due in next fortnight
+      org-agenda-span 14
+      ;;don't show tasks as scheduled if they are already shown as a deadline
+      ;; org-agenda-skip-scheduled-if-deadline-is-shown t
+
+      ;; timestamp when we set something to done
+      ;; org-log-done nil
+      org-agenda-start-with-log-mode t
+      org-log-into-drawer t
+
+
+      org-journal-date-format "%B %d, %Y - %A"
+      org-journal-file-format "%Y%m.org"
+      org-journal-file-type 'monthly
+      org-journal-enable-agenda-integration t
+      ;; org-journal-dir "~/org/journal/"
+
+      ;; org-default-notes-file "~/org/inbox.org"
+      ;; org-agenda-start-on-weekday 1 ;; start on monday instead of current day
+      ;; org-use-fast-todo-selection t
+
+      ;; org-refile-targets '((nil :maxlevel . 4)
+      ;;                      (org-agenda-files :maxlevel . 4))
+
+      ;; include the file in the refile search
+      org-refile-use-outline-path 'file
+      ;; org-deadline-warning-days 7
+      ;; org-agenda-compact-blocks nil ;; don't compact the agenda
+      ;; give me all the possible completions at once so helm can present them
+      ;; org-outline-path-complete-in-steps nil)
+      ;; org-agenda-default-appointment-duration 60
+      ;; org-agenda-skip-scheduled-if-done t
+
+      ;; I don't think all are added if not
+      org-agenda-files (directory-files-recursively "~/org/" "\\.org$")
+
+      ;; org-agenda-clockreport-parameter-plist
+      ;; (quote (:link t :maxlevel 5 :fileskip0 t :compact t :narrow 80))
+
+      org-habit-graph-column 60
+
+      +org-capture-todo-file "inbox.org"
+)
+(advice-add 'org-refile :after 'org-save-all-org-buffers)
+
