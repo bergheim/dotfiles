461d1af4fe8af8edc9626051de979dc23f1edf43
Thomas Bergheim <tbergheim@gmail.com>
2021-12-30 15:06:17 +0100

    emacs: replace ivy with vertico
    It seems to work nice, and embark and marginalia is helpful

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -23,10 +23,11 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy               ; a search engine for love and life
-        ;; +fuzzy
-        ;; +prescient
-        +icons)
+       ;; (ivy               ; a search engine for love and life
+       ;;  ;; +fuzzy
+       ;;  ;; +prescient
+       ;;  +icons)
+       (vertico +icons)           ; the search engine of the future
 
        :ui
        deft              ; notational velocity for Emacs
