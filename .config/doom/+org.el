501b2d44e17cf237ccffafc9550bd00302b9c602
Thomas Bergheim <tbergheim@gmail.com>
2022-01-09 21:15:32 +0100

    emacs(org): set up custom agenda commands
    Finally. dashboard, overview, specific searches etc..

--- .config/doom/+org.el
+++ .config/doom/+org.el
@@ -119,6 +119,180 @@
                   "|" "DONE(d@)" "CANCELLED(c@/!)")
         (sequence "BUG(b)" "|" "FIXED(f!)" "IGNORED(x@/!)")))
 
+;; (use-package! org-super-agenda
+;;   :after org-agenda
+;;   :commands org-super-agenda-mode
+;;   :config
+;;   (setq org-super-agenda-header-map (make-sparse-keymap)))
+
+(org-super-agenda-mode)
+;; don't break evil on org-super-agenda headings, see
+;; https://github.com/alphapapa/org-super-agenda/issues/50
+(setq org-super-agenda-header-map (make-sparse-keymap))
+
+;; (setq org-agenda-prefix-format
+;;       (quote
+;;        ((agenda . "%-12c%?-18t% s")
+;;         (timeline . "% s")
+;;         (todo . "%-20c")
+;;         (tags . "%-22c")
+;;         (search . "%-12c"))))
+
+(setq org-agenda-custom-commands
+      '(("d" "Dashboard"
+         ((agenda "" ((org-agenda-overriding-header "Dashboard")
+                      (org-agenda-span 'day)
+                      ;; (org-agenda-current-span 'day)
+                      (org-agenda-start-day (org-today))
+                      (org-super-agenda-groups
+                       '((:name "First, do one of these"
+                          ;; :discard (:not (:tag "@work"))
+                          :and (:deadline today :priority "A")
+                          :deadline today
+                          :and (:deadline past :priority "A")
+                          :and (:scheduled t :priority "A")
+                          :and (:scheduled past :priority "A")
+                          :deadline past
+                          :order 1)
+                         (:name "And make sure you keep up these :)"
+                          :and (:scheduled today :habit t)
+                          :order 2)
+                         (:name "Now look at your day"
+                          :time-grid t
+                          :order 3)
+                         (:name "Do you still need to do these?"
+                          ;; :discard (:tag ("someday" "habits"))
+                          :scheduled past
+                          :order 10)
+                         (:name "Upcoming deadlines"
+                          :deadline future
+                          :order 3)))))
+          (alltodo "" ((org-agenda-overriding-header "")
+                       (org-super-agenda-groups
+                        '((:name "You need to act on these"
+                           ;; :discard (:not (:tag "@work"))
+                           :deadline future
+                           :order 3)
+                          (:name "And maybe start on these?"
+                           :discard (:tag "habits")
+                           :scheduled future
+                           :order 4)
+                          (:discard (:anything t))))))))
+
+        ("w" "Work related tasks"
+         ((tags-todo "@work|planet9" (
+                                      (org-super-agenda-groups
+                                       '(
+                                         ;; (:discard (:not (:and (:tag ("@work" "planet9")))))
+                                         (:name "Important tasks"
+                                          :priority ("A" "B")
+                                          :order 1)
+                                         (:name "Needs refiling"
+                                          :tag "REFILE"
+                                          :order 1)))))))
+
+        ("c" "Done and clocked items"
+         ((agenda "" ((org-agenda-overriding-header "")
+                      (org-agenda-span 'day)
+                      (org-agenda-current-span 'week)
+                      (org-agenda-start-day (org-today))
+                      (org-super-agenda-groups
+                       '((:name "Done today"
+                          :and (:regexp "State \"DONE\""
+                                :log t))
+                         (:name "Clocked today"
+                          :log t)
+                         (:discard (:anything t))))))))
+
+        ("i" "In progress" tags-todo "TODO=\"INPROGRESS\"")
+        ("l" "Low effort tasks" tags-todo "EFFORT>=\"0:01\"&EFFORT<=\"0:15\"")
+
+        ("p" "Projects" tags "+project-someday-TODO=\"DONE\"-TODO=\"SOMEDAY\""
+         ((org-tags-exclude-from-inheritance '("project"))
+          (org-agenda-sorting-strategy '(priority-down tag-up category-keep effort-down))))
+
+        ("O" "Timeline for today" ((agenda "" ))
+         ((org-agenda-ndays 1)
+          (org-agenda-show-log t)
+          (org-agenda-log-mode-items '(clock closed))
+          (org-agenda-clockreport-mode t)
+          (org-agenda-entry-types '())))
+
+        ("gc" "Coding" tags-todo "@coding"
+         ((org-agenda-view-columns-initially t)))
+        ("gh" "Home" tags-todo "@home"
+         ((org-agenda-view-columns-initially t)))
+        ("ge" "Errands" tags-todo "@errands"
+         ((org-agenda-view-columns-initially t)))
+        ("gw" "Waiting for" todo "WAITING")
+        ("gs" "Someday" tags-todo "TODO=\"SOMEDAY\""
+         ((org-agenda-view-columns-initially nil)
+          (org-tags-exclude-from-inheritance '("project"))
+          (org-agenda-overriding-header "Someday: ")
+          (org-columns-default-format "%50ITEM %TODO %3PRIORITY %Effort{:} %TAGS")
+          (org-agenda-sorting-strategy '(todo-state-up priority-down effort-up tag-up category-keep))))
+        ("gi" "In progress" tags-todo "TODO=\"INPROGRESS\"")
+        ("gP" "By priority"
+         ((tags-todo "+PRIORITY=\"A\"")
+          (tags-todo "+PRIORITY=\"B\"")
+          (tags-todo "+PRIORITY=\"\"")
+          (tags-todo "+PRIORITY=\"C\""))
+         ((org-agenda-prefix-format "%-10c %-10T %e ")
+          (org-agenda-sorting-strategy '(priority-down tag-up category-keep effort-down))))
+
+        ("o" "Overview"
+         ((agenda "Agenda today" ((org-agenda-span 'day)
+                                  (org-super-agenda-groups
+                                   '((:name "Today"
+                                      :time-grid t
+                                      :date today
+                                      :todo "TODAY"
+                                      :scheduled today
+                                      :order 1)))))
+          (alltodo "All todos" ((org-agenda-overriding-header "")
+                                (org-super-agenda-groups
+                                 '((:name "Next to do"
+                                    :todo "NEXT"
+                                    :order 1)
+                                   (:name "Important"
+                                    :tag "Important"
+                                    :priority "A"
+                                    :order 6)
+                                   (:name "Due Today"
+                                    :deadline today
+                                    :order 2)
+                                   (:name "Due Soon"
+                                    :deadline future
+                                    :order 8)
+                                   (:name "Overdue"
+                                    :deadline past
+                                    :face error
+                                    :order 7)
+                                   (:name "Projects"
+                                    :tag "Project"
+                                    :order 14)
+                                   (:name "Emacs"
+                                    :tag "emacs"
+                                    :order 13)
+                                   (:name "To read"
+                                    :tag "toread"
+                                    :order 30)
+                                   (:name "Waiting"
+                                    :todo "WAITING"
+                                    :order 20)
+                                   (:discard (:tag ("Chore" "Routine" "Daily")))))))))
+
+        ("D" "Playground"
+         ((tags "test"
+                ((org-agenda-overriding-header "Work things\n")))
+          (agenda ""
+                  ((org-agenda-overriding-header "Todays agenda")
+                   (org-agenda-block-separator ?*)
+                   (org-deadline-warning-days 0)
+                   (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+                   ;; (org-super-agenda-date-format "%A %-e %B %Y")
+                   (org-agenda-span 1))
+                  )))))
 
 ;; thanks tecosaur
 (defun +doct-icon-declaration-to-icon (declaration)
