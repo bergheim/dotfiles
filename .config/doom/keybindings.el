8a363922037be38ac713ca09f3d18680ffc70056
Thomas Bergheim <tbergheim@gmail.com>
2022-01-29 02:19:57 +0100

    emacs: add org-ql and dashboard shortcuts

--- .config/doom/keybindings.el
+++ .config/doom/keybindings.el
@@ -16,8 +16,6 @@
        "d" 'magit-diff-buffer-file
        "l" 'magit-log-buffer-file)
 
-     :desc "org inbox" "i" (lambda () (interactive) (find-file "~/org/inbox.org"))
-
      (:prefix ("o" . "org")
        "a" 'org-agenda
        "g" 'org-clock-goto
@@ -44,16 +42,16 @@
 
      (:prefix ("d" . "dotfiles")
       (:desc "Magit" "d" (λ! (magit-status "/yadm::")))
-      (:desc "Find a file" "f" 'find-in-dotfiles)
-      (:desc "Browse" "b" 'browse-dotfiles))
+      (:desc "Find a file" "f" 'bergheim/find-in-dotfiles)
+      (:desc "Browse" "b" 'bergheim/browse-dotfiles))
 
      (:prefix ("m" . "E-mail")
       "m" '=mu4e
       ;; TODO figure out how to load mu4e if this is called
       (:desc "Compose" "c" #'+mu4e/compose)
       (:desc "Search" "s" #'mu4e-headers-search)
-      (:desc "Sent" "S" 'bergheim-email-sent)
-      (:desc "Today's email" "t" 'bergheim-email-today)
+      (:desc "Sent" "S" 'bergheim/mu4e-email-sent)
+      (:desc "Today's email" "t" 'bergheim/mu4e-email-today)
       (:desc "Today's unhandled email" "T" 'bergheim-email-today-or-unread)
       (:desc "Today's work email" "w" 'bergheim-email-work-inbox)
       (:desc "Today's personal email" "p" 'bergheim-email-personal-inbox)
@@ -68,5 +66,9 @@
 
      "r" 'rainbow-mode
 
+     (:desc "Orgmode Dashboard" "a" (lambda (&optional arg) (interactive "P") (org-agenda arg "d")))
+     (:desc "Email Dashboard" "s" 'bergheim/mu4e-email-today)
+     (:desc "Orgmode Recent" "r" 'bergheim/org-agenda-recent-changes)
+     (:desc "Orgmode Work" "w" 'bergheim/org-agenda-work-items)
      "t" 'heaven-and-hell-toggle-theme
      "T" 'heaven-and-hell-load-default-theme)))
