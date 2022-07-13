697faff7d964de69a31d9239308c07f46f862c7f
Thomas Bergheim <tbergheim@gmail.com>
2022-07-14 00:41:55 +0200

    emacs: add back the yadm helper
    I don't really use it though since you can just use TRAMP. But it was there..

--- .config/doom/autoload/bergheim.el
+++ .config/doom/autoload/bergheim.el
@@ -13,6 +13,23 @@
   (doom-project-browse (expand-file-name "~/.config/")))
 
 ;;;###autoload
+(defun bergheim-toggle-yadm ()
+  "Toggle the GIT_DIR between nil and yadm. Opens magit-status when it is enabled. Prefer TRAMP to this"
+  (interactive)
+  ;; use a property “state”. Value is t or nil
+  (if (get 'tsb-toggle-yadm 'state)
+      (progn
+        (message "Disabling YADM")
+        (setenv "GIT_DIR" nil)
+        (put 'tsb-toggle-yadm 'state nil))
+    (progn
+      ;; TODO enable the XDG env for this
+      (message (concat "Enabling YADM " (getenv "HOME") "/.local/share/yadm/repo.git"))
+      (setenv "GIT_DIR" (concat (getenv "HOME") "/.local/share/yadm/repo.git"))
+      (put 'tsb-toggle-yadm 'state t)
+      (magit-status))))
+
+;;;###autoload
 (defun bergheim-email-inbox()
   (interactive)
   (mu4e-headers-search-bookmark "maildir:/Inbox/"))
