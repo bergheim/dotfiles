d5251df214339297da54fc065c205a2ac407e6e8
Thomas Bergheim <tbergheim@gmail.com>
2022-07-14 00:49:02 +0200

    emacs: add tao colorless theme

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -46,8 +46,9 @@
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 
-(package! org-mru-clock)
+(package! tao-theme) ;; uncolored back and white themes
 
+(package! org-mru-clock)
 (package! org-super-agenda)
 (package! org-ql)
 
