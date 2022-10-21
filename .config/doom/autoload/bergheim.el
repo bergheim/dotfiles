489305d6d47746c034bfe3c55b8a335971406184
Thomas Bergheim <tbergheim@gmail.com>
2022-10-21 15:53:07 +0200

    emacs(mu4e): update deprecated function names

--- .config/doom/autoload/bergheim.el
+++ .config/doom/autoload/bergheim.el
@@ -32,50 +32,50 @@
 ;;;###autoload
 (defun bergheim/email-inbox()
   (interactive)
-  (mu4e-headers-search-bookmark "maildir:/Inbox/"))
+  (mu4e-search-bookmark "maildir:/Inbox/"))
 
 ;;;###autoload
 (defun bergheim/email-sent()
   (interactive)
-  (mu4e-headers-search-bookmark "maildir:/Sent/"))
+  (mu4e-search-bookmark "maildir:/Sent/"))
 
 ;;;###autoload
 (defun bergheim/email-trash()
   (interactive)
-  (mu4e-headers-search-bookmark "maildir:/Trash/ OR flag:trashed"))
+  (mu4e-search-bookmark "maildir:/Trash/ OR flag:trashed"))
 
 ;;;###autoload
 (defun bergheim/email-important()
   (interactive)
-  (mu4e-headers-search-bookmark "(maildir:/Inbox/ AND date:1w..now AND flag:unread) OR flag:flagged"))
+  (mu4e-search-bookmark "(maildir:/Inbox/ AND date:1w..now AND flag:unread) OR flag:flagged"))
 
 ;;;###autoload
 (defun bergheim/email-work-inbox()
   (interactive)
-  (mu4e-headers-search-bookmark "maildir:/neptune/Inbox/"))
+  (mu4e-search-bookmark "maildir:/neptune/Inbox/"))
 
 ;;;###autoload
 (defun bergheim/email-personal-inbox()
   (interactive)
-  (mu4e-headers-search-bookmark "(maildir:/glvortex/Inbox/ OR maildir:/gmail/Inbox) AND (date:1w..now OR flag:unread)"))
+  (mu4e-search-bookmark "(maildir:/glvortex/Inbox/ OR maildir:/gmail/Inbox) AND (date:1w..now OR flag:unread)"))
 
 ;;;###autoload
 (defun bergheim/email-today()
   "Opens the inbox with unread and todays email"
   (interactive)
-  (mu4e-headers-search-bookmark "(date:1d..now) AND maildir:/Inbox/"))
+  (mu4e-search-bookmark "(date:1d..now) AND maildir:/Inbox/"))
 
 ;;;###autoload
 (defun bergheim/email-week()
   "Opens the inbox with unread and this weeks email"
   (interactive)
-  (mu4e-headers-search-bookmark "(date:1w..now) AND maildir:/Inbox/"))
+  (mu4e-search-bookmark "(date:1w..now) AND maildir:/Inbox/"))
 
 ;;;###autoload
 (defun bergheim/email-today-or-unread()
   "Opens the inbox with unread and todays email"
   (interactive)
-  (mu4e-headers-search-bookmark "maildir:/Inbox/ AND (date:1d..now OR flag:unread)"))
+  (mu4e-search-bookmark "maildir:/Inbox/ AND (date:1d..now OR flag:unread)"))
 
 ;;;###autoload
 (defun bergheim/email-spam()
