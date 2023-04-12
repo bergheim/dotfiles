44ae36d219468f4c853e93a7fcb6fd46cc294f13
Thomas Bergheim <tbergheim@gmail.com>
2023-04-12 21:05:12 +0200

    emacs(doom): add elixir, tree-sitter

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -126,7 +126,7 @@
        data                ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
-       ;;elixir            ; erlang done right
+       (elixir +lsp +tree-sitter)            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp          ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
@@ -184,7 +184,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp +tree-sitter) ; the tubes
-       yaml              ; JSON, but readable
+       (yaml +tree-sitter) ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
