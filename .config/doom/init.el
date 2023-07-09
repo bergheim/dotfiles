95e5992426b9882477f97717c66cc6c396a98575
Thomas Bergheim <tbergheim@gmail.com>
2023-07-09 13:25:43 +0200

    emacs: add lsp/poetry support for python

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -168,7 +168,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +tree-sitter) ; beautiful is better than ugly
+       (python +tree-sitter +lsp +poetry) ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
