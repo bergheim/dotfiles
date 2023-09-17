dfaf5322a41ea0b7fa0e2f48cea6ff41b3c1e136
Thomas Bergheim <tbergheim@gmail.com>
2023-09-17 16:16:12 +0200

    neomacs: add a bunch of keybindings
    org-agenda bindings still needs fixing..

--- .config/neodoom/modules/keybindings.el
+++ .config/neodoom/modules/keybindings.el
@@ -28,10 +28,38 @@
 
 (use-package general
   :ensure t
+  ;; :after evil-org-agenda
   :config
+  ;; TODO: make this work with reloads
+  ;; (general-unbind :prefix "SPC")
+
+  (general-def
+    :keymaps '(evil-normal-state-map org-agenda-mode-map)
+    "C-S-u" 'universal-argument
+    "C-u" 'evil-scroll-up)
+
+
+  ;; (general-unbind 'org-agenda-mode-map
+  ;;   "SPC")
+  ;; (general-create-definer my-leader-def
+  ;;   :prefix "SPC")
+  ;; (my-leader-def
+  ;;  :states '(normal visual motion)
+  ;;  :prefix "SPC"
+  ;;  :keymaps 'org-agenda-mode-map
+  ;;  "bb" '(switch-to-buffer :which-key "Switch"))
+
+  ;; (general-def 'emacs org-agenda-mode-map
+  ;;  ;; :states 'motion
+  ;;  :prefix "SPC"
+  ;;  ;; :keymaps '(emacs evil-normal-state-map org-agenda-mode-map)
+  ;;  :non-normal-prefix "M-SPC"
+  ;;  "b" '(:ignore t :which-key "Buffers"))
+
   (general-define-key
-   :states '(normal visual insert emacs)
+   :states '(normal visual insert emacs motion)
    :prefix "SPC"
+   :keymaps '(global-map org-agenda-mode-map)
    :non-normal-prefix "M-SPC"
 
    "b" '(:ignore t :which-key "Buffers")
@@ -50,8 +78,9 @@
    "hf" '(helpful-callable :which-key "Function")
    "hk" '(helpful-key :which-key "Key")
    "ha" '(apropos :which-key "Apropos")
-   "hk" '(describe-mode :which-key "Desibe mode")
+   "hm" '(describe-mode :which-key "Describe mode")
    "hr" '(bergheim/reload-init-file :which-key "Reload")
+   "ht" '(consult-theme :which-key "Switch theme")
 
    "o" '(:ignore t :which-key "Org Mode")
    "od" '((lambda (&optional arg) (interactive) (org-agenda arg "d")) :which-key "Orgmode Dashboard")
@@ -63,49 +92,74 @@
    "pp" '(projectile-switch-project :which-key "Switch project")
    "pu" '(projectile-discover-projects-in-search-path :which-key "Update projects")
 
+   ;; TODO: add lots more consult stuff like kill-ring etc
+   "s" '(:ignore t :which-key "Search")
+   "sa" '(consult-org-agenda :which-key "org agenda")
+   "sb" '(consult-line-multi :which-key "open buffers")
+   "sh" '(consult-org-heading :which-key "org heading")
+   "sh" '(consult-recent-file :which-key "recent files")
+   "sI" '(consult-imenu-multi :which-key "imenu items in all buffers")
+   "si" '(consult-imenu :which-key "imenu items")
+   "sm" '(consult-man :which-key "man")
+   "sp" '(consult-project-buffer :which-key "project buffers")
+   "ss" '(consult-line :which-key "buffer")
+
    "m" '(:ignore t :which-key "Mode specific")
 
    "q" '(:ignore t :which-key "Quit")
    "qq" '(save-buffers-kill-terminal :which-key "Quit")
    "qr" '(restart-emacs :which-key "Restart"))
 
-  (general-define-key
-   :states '(normal insert)
-   "M-h" #'evil-window-left
-   "M-j" #'evil-window-down
-   "M-k" #'evil-window-up
-   "M-l" #'evil-window-right
-   "M-H" #'+evil/window-move-left
-   "M-J" #'+evil/window-move-down
-   "M-K" #'+evil/window-move-up
-   "M-L" #'+evil/window-move-right
-   "M-\\" #'evil-window-vsplit
-   "M-]" #'evil-window-split
-   "M-DEL" #'evil-window-delete
-   ;; "M-<backspace>" #'+workspace/close-window-or-workspace
-   "M-S-<backspace>" #'kill-current-buffer
-   "M-o" #'evil-window-next
-   "M-f" #'maximize-window
-   "M-F" #'winner-undo)
 
-;; (general-define-key
-;;  :states 'motion
-;;  :keymaps 'org-agenda-mode-map
-;;  "M-h" nil)
+  (defun my-define-common-keys (&optional keymap states)
+    (let ((target-keymap (or keymap 'global)))
+      (general-define-key
+       :states (or states '(normal insert))
+       :keymaps target-keymap
+       "M-h" #'evil-window-left
+       "M-j" #'evil-window-down
+       "M-k" #'evil-window-up
+       "M-l" #'evil-window-right
+
+       "M-H" #'+evil/window-move-left
+       "M-J" #'+evil/window-move-down
+       "M-K" #'+evil/window-move-up
+       "M-L" #'+evil/window-move-right
+
+       "M-\\" #'evil-window-vsplit
+       "M-]" #'evil-window-split
+
+       "M-DEL" #'evil-window-delete
+       ;; "M-<backspace>" #'+workspace/close-window-or-workspace
+       "M-S-<backspace>" #'kill-current-buffer
+
+       "M-o" #'evil-window-next
+       "M-f" #'maximize-window
+       "M-F" #'winner-undo
+       )))
+
+  (my-define-common-keys)
+
+  ;; ;; Apply to org-agenda-mode-map for normal, insert, and visual states:
+  ;; (my-define-common-keys 'org-agenda-mode-map '(normal insert visual))
+
+  (my-define-common-keys '(org-mode-map org-agenda-mode-map) 'motion)
+
+
+  ;; (general-define-key
+  ;;  :states 'motion
+  ;;  :keymaps 'org-agenda-mode-map
+  ;;  :prefix "SPC"
+  ;;  :non-normal-prefix "M-SPC"
+
+  ;;  "b" '(:ignore t :which-key "Buffers")
+  ;;  "bb" '(switch-to-buffer :which-key "Switch"))
+
 
   (general-define-key
    :states 'motion
-   :keymaps 'org-agenda-mode-map
-   "M-h" #'evil-window-left
-   "M-j" #'evil-window-down
-   "M-k" #'evil-window-up
-   "M-l" #'evil-window-right
-
-   "M-H" #'+evil/window-move-left
-   "M-J" #'+evil/window-move-down
-   "M-K" #'+evil/window-move-up
-   "M-L" #'+evil/window-move-right
-
+   :keymaps 'org-mode-map
+   ;; TODO: do I still use these
    "C-M-h" #'org-metaleft
    "C-M-j" #'org-metadown
    "C-M-k" #'org-metaup
@@ -114,19 +168,7 @@
    "C-M-S-h" #'org-shiftmetaleft
    "C-M-S-j" #'org-shiftmetadown
    "C-M-S-k" #'org-shiftmetaup
-   "C-M-S-l" #'org-shiftmetaright
-
-   "M-\\" #'evil-window-vsplit
-   "M-]"  #'evil-window-split
-   "M-DEL" #'evil-window-delete
-   "M-<backspace>" #'+workspace/close-window-or-workspace
-   ))
-
-  ;; (general-define-key
-  ;;  :states 'normal
-  ;;  :package 'evil-org-agenda
-  ;;  :keymaps 'evil-org-agenda-mode-map
-  ;;  "M-h" #'evil-window-left)
+   "C-M-S-l" #'org-shiftmetaright))
 
 (general-create-definer bergheim/emacs-lisp-keys
   :prefix "SPC m"
