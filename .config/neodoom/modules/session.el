cddb66fcadf9fc207d71f01fac6638b65f69e5e6
Thomas Bergheim <thomas.bergheim@neptune-software.com>
2023-09-19 17:11:24 +0200

    neomacs: prefer a simple session restore

--- .config/neodoom/modules/session.el
+++ .config/neodoom/modules/session.el
@@ -1,40 +1,28 @@
 ;;; session.el --- Description -*- lexical-binding: t; -*-
-;;
+
 (use-package desktop
+  :defer t
   :init
-  ;; Store desktop files in `bergheim/cache-dir`.
-  (setq desktop-dirname (expand-file-name "desktop/" bergheim/cache-dir)
-        desktop-base-file-name "emacs.desktop"
-        desktop-base-lock-name "lock"
-        desktop-path (list desktop-dirname)
-        desktop-save t
-        desktop-load-locked-desktop nil) ; Don't load desktops that are locked.
+  ;; Define the path for desktop files.
+  (setq desktop-path (list bergheim/cache-dir)
+        desktop-auto-save-timeout 30
+        desktop-save 'if-exists
+        desktop-load-locked-desktop t) ; Load even if there's a lock.
 
-  ;; Create the directory if it doesn't exist.
-  (unless (file-exists-p desktop-dirname)
-    (make-directory desktop-dirname t))  ; The `t` argument creates parent directories as needed.
-
-  ;; Auto-save and load the desktop.
-  :config
-  (desktop-save-mode 1)
-
-  ;; Ensure the desktop is saved before quitting.
-  (add-hook 'kill-emacs-hook (lambda ()
-                               (desktop-save-in-desktop-dir))))
+  ;; Auto-save and load without prompting.
+  (setq desktop-save-mode 1)
+  (add-hook 'desktop-after-read-hook (lambda () (desktop-save-mode 1)))
+  (desktop-read bergheim/cache-dir))
 
 (use-package restart-emacs
   :ensure t
   :after desktop
   :config
-  ;; Ensure desktop saving occurs before `restart-emacs` is invoked.
-  (advice-add 'restart-emacs :before 'desktop-save-in-desktop-dir)
-
-  ;; TODO: remove this once I move off the init stuff
-  (defun bergheim/restart-emacs--translate-current-executable-advice (orig-fun &rest args)
-    (let ((result (apply orig-fun args)))
-      (list result "--init-directory" "~/.config/neodoom" "--debug-init")))
-
-  (advice-add 'restart-emacs--translate-current-executable
-              :around 'bergheim/restart-emacs--translate-current-executable-advice))
+  (defun bergheim/restart-emacs ()
+    "Save desktop and then restart Emacs with custom init directory."
+    (interactive)
+    (desktop-save bergheim/cache-dir)
+    (let ((restart-args `("--init-directory" ,(expand-file-name "~/.config/neodoom"))))
+      (apply #'restart-emacs restart-args))))
 
 ;;; session.el ends here
