d158c67273d1aa5dd3a139a6936168999fb736c5
Thomas Bergheim <tbergheim@gmail.com>
2024-01-26 20:55:05 +0100

    neomacs: enable apheleia globally

--- .config/neodoom/modules/formating.el
+++ .config/neodoom/modules/formating.el
@@ -2,14 +2,14 @@
 ;;
 
 (use-package apheleia
+  :init
+  (apheleia-global-mode 1)
   :config
   ;; temp fix while apheleia does not support this ts mode directly
   (setf (alist-get 'prettier apheleia-formatters)
         '("prettier" "--stdin-filepath" filepath))
   (setf (alist-get 'typescript-ts-mode apheleia-mode-alist)
-        'prettier)
-
-  (apheleia-global-mode +1))
+        'prettier))
 
 (provide 'formating)
 ;;; formating.el ends here
