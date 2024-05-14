d35af6a5dd5e8b982af79abd45b86d11b9d2c280
Thomas Bergheim <tbergheim@gmail.com>
2024-05-14 16:14:42 +0200

    emacs: format mu4e autoload

--- .config/emacs/autoloads/email.autoload.el
+++ .config/emacs/autoloads/email.autoload.el
@@ -2,53 +2,53 @@
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-today
-                       "Opens the inbox with unread and today's email."
-                       "maildir:/Inbox/ AND date:2d..now")
+  "Opens the inbox with unread and today's email."
+  "maildir:/Inbox/ AND date:1m..now")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-inbox
-                       "Opens the entire inbox"
-                       "maildir:/Inbox/")
+  "Opens the entire inbox"
+  "maildir:/Inbox/")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-important
-                       "Important email"
-                       "flag:flagged OR (maildir:/Inbox/ AND flag:unread AND date:1w..now)")
+  "Important email"
+  "flag:flagged OR (maildir:/Inbox/ AND flag:unread AND date:1w..now)")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-inbox-work
-                       "Work email"
-                       "maildir:/neptune/Inbox/")
+  "Work email"
+  "maildir:/neptune/Inbox/")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-inbox-personal
-                       "Personal email"
-                       "(maildir:/glvortex/Inbox/ OR maildir:/gmail/Inbox) AND (flag:unread OR date:1w..now)")
+  "Personal email"
+  "(maildir:/glvortex/Inbox/ OR maildir:/gmail/Inbox) AND (flag:unread OR date:1w..now)")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-today-or-unread
-                       "Opens the inbox with unread and today's email"
-                       "maildir:/Inbox/ AND (flag:unread OR date:2d..now)")
+  "Opens the inbox with unread and today's email"
+  "maildir:/Inbox/ AND (flag:unread OR date:2d..now)")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-week
-                       "Opens the inbox with unread and this weeks email"
-                       "maildir:/Inbox/ AND date:1w..now")
+  "Opens the inbox with unread and this weeks email"
+  "maildir:/Inbox/ AND date:1w..now")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-sent
-                       "Sent email"
-                       "maildir:/Sent/")
+  "Sent email"
+  "maildir:/Sent/")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-trash
-                       "Trash"
-                       "maildir:/Trash/ OR flag:trashed")
+  "Trash"
+  "maildir:/Trash/ OR flag:trashed")
 
 ;;;###autoload
 (define-mu4e-search-fn bergheim/email-junk
-                       "Show the junk mail from all accounts"
-                       "maildir:/Spam/")
+  "Show the junk mail from all accounts"
+  "maildir:/Spam/")
 
 ;;;###autoload
 (defun bergheim/lookup-anything ()
