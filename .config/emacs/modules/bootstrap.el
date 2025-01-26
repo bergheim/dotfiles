6ee18b93e96ffad04811e6fbb184c42c1fb7c6dc
Thomas Bergheim <knighthk@glvortex.net>
2025-01-26 01:35:46 +0100

    emacs(undo-tree): simplify keybinds

--- .config/emacs/modules/bootstrap.el
+++ .config/emacs/modules/bootstrap.el
@@ -101,26 +101,21 @@
 (use-package git-auto-commit-mode
   :demand t)
 
+;; general modifies use-package so make sure we get it before moving on
+(elpaca-wait)
+
 ;; as long as this doesn't destroy my data..
 (use-package undo-tree
   :demand t
+  :after general
   :init
   (global-undo-tree-mode)
   :config
   (setq undo-tree-auto-save-history t)
   (setq undo-tree-enable-undo-in-region t)
+  :general
+  (general-nmap
+    "u" 'undo-tree-undo
+    "U" 'undo-tree-visualize
+    "C-r" 'undo-tree-redo))
 
-  ;; TODO: For some reason, general.el isn't binding 'U' as expected.
-  ;; Using define-key as a workaround.
-  (define-key evil-normal-state-map (kbd "u") 'undo-tree-undo)
-  (define-key evil-normal-state-map (kbd "U") 'undo-tree-visualize)
-  (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo))
-
-;; :general
-;; (general-nmap
-;;   "u" 'undo-tree-undo
-;;   "U" 'undo-tree-visualize
-;;   "C-r" 'undo-tree-redo))
-
-;; general modifies use-package so make sure we get it before moving on
-(elpaca-wait)
