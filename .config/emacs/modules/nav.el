e9998f0ff97cd817469a1b6998560edf4774594c
Thomas Bergheim <knighthk@glvortex.net>
2025-03-22 03:39:30 +0100

    emacs(eshell): M-RET opens eshell from current dired session

--- .config/emacs/modules/nav.el
+++ .config/emacs/modules/nav.el
@@ -167,6 +167,7 @@ With a universal argument, it allows entering the application to use."
    "O"   #'dirvish-quicksort
    "TAB" #'dirvish-subtree-toggle
    "C-<return>" #'bergheim/dired-return-path
+   "M-<return>" #'bergheim/dired-leave-for-eshell
    "C-h" #'dirvish-history-go-backward
    "C-l" #'dirvish-history-go-forward
    "C-M-k" #'dirvish-emerge-previous-group
@@ -196,7 +197,16 @@ With a universal argument, it allows entering the application to use."
             (call-interactively 'bergheim/org-attach-dired-to-subtree))
           :which-key "Copy to org")
     "O" `(,(bergheim/call-with-universal-arg #'bergheim/org-attach-dired-to-subtree)
-          :which-key "Move to org")))
+          :which-key "Move to org"))
+
+  (defun bergheim/dired-leave-for-eshell ()
+    "Quit dirvish and open eshell in the current directory."
+    (interactive)
+    (let ((current-dir default-directory))
+      (dirvish-quit)
+      (eshell)
+      (eshell/cd current-dir)
+      (eshell-send-input))))
 
 (defun bergheim/delete-current-file ()
   "Delete the current file and kill its buffer, after asking for confirmation."
