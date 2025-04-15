1daa2a28458b7c44fa3c21343ff5a874033572b9
Thomas Bergheim <spammers@glvortex.net>
2025-04-15 13:02:02 +0200

    emacs(elpaca): update syntax for latest version

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -18,7 +18,7 @@
                       (expand-file-name "~/"))))
     xdg-home))
 
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca" bergheim/cache-dir))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -53,7 +53,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
