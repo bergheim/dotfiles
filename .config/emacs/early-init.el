3bf6caa385ff8d0ec47a0ac7f7daf52b9999e3d6
Thomas Bergheim <spammers@glvortex.net>
2025-05-30 13:46:54 +0200

    Emacs: don't set colors on startup. Fixes error-on-startup readability

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -38,15 +38,6 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
 (setq default-frame-alist '((fullscreen . maximized)
-
-                            ;; You can turn off scroll bars by uncommenting these lines:
-                            ;; (vertical-scroll-bars . nil)
-                            ;; (horizontal-scroll-bars . nil)
-
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (ns-appearance . dark)
                             (ns-transparent-titlebar . t)))
 
 
