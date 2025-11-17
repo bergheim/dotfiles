657877a2002281ba25d3eece0581a1f0113f3aee
Thomas Bergheim <knighthk@glvortex.net>
2025-11-17 11:57:14 +0100

    emacs(persistent-scratch): RIP (sounds good, wasn't using it)

--- .config/emacs/modules/base.el
+++ .config/emacs/modules/base.el
@@ -248,13 +248,6 @@
   :config
   (setq flymake-temporary-file-directory (bergheim/get-and-ensure-data-dir "flymake")))
 
-(use-package persistent-scratch
-  :unless bergheim/container-mode-p
-  :ensure t
-  :demand t
-  :config
-  (persistent-scratch-setup-default))
-
 (use-package pdf-tools
   :unless bergheim/container-mode-p
   :defer t
