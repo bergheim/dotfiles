16ee8749167b20936dccaaf7ac38d96c964d07a9
Thomas Bergheim <knighthk@glvortex.net>
2026-05-27 09:53:22 +0200

    nvim: format init file

--- .config/nvim/lua/bergheim/init.lua
+++ .config/nvim/lua/bergheim/init.lua
@@ -74,52 +74,52 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- vim.lsp.set_log_level("debug")
 
 local function copy_current_file()
-    local current_file = vim.fn.expand("%:p")
-    if current_file == "" then
-        print("No file to copy")
-        return
-    end
+	local current_file = vim.fn.expand("%:p")
+	if current_file == "" then
+		print("No file to copy")
+		return
+	end
 
-    local new_file = vim.fn.input("Copy to: ", current_file, "file")
-    if new_file == "" then
-        print("Copy cancelled")
-        return
-    end
+	local new_file = vim.fn.input("Copy to: ", current_file, "file")
+	if new_file == "" then
+		print("Copy cancelled")
+		return
+	end
 
-    vim.fn.writefile(vim.fn.readfile(current_file), new_file)
-    print("File copied to " .. new_file)
+	vim.fn.writefile(vim.fn.readfile(current_file), new_file)
+	print("File copied to " .. new_file)
 end
 
 -- Keybinding to copy the current file
 vim.keymap.set("n", "<leader>fc", copy_current_file, { desc = "Copy current file" })
 
 local function delete_current_file()
-    local file = vim.fn.expand("%")
-    if file == "" then
-        print("No file to delete")
-        return
-    end
+	local file = vim.fn.expand("%")
+	if file == "" then
+		print("No file to delete")
+		return
+	end
 
-    -- Ask for confirmation
-    local response = vim.fn.confirm("Are you sure you want to delete " .. file .. "?", "&Yes\n&No", 2)
+	-- Ask for confirmation
+	local response = vim.fn.confirm("Are you sure you want to delete " .. file .. "?", "&Yes\n&No", 2)
 
-    if response == 1 then
-        vim.fn.delete(file)
-        vim.cmd("bdelete!")
-        print("Deleted " .. file)
-    else
-        print("Deletion cancelled")
-    end
+	if response == 1 then
+		vim.fn.delete(file)
+		vim.cmd("bdelete!")
+		print("Deleted " .. file)
+	else
+		print("Deletion cancelled")
+	end
 end
 
 -- Bind the function to <leader>fD
