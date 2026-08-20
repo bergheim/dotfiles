58da6bdee4c85b66b96c3be3bc13e734a8ad0be7
Thomas Bergheim <knighthk@glvortex.net>
2026-08-20 14:45:24 +0200

    yay: set secure defaults

---
+++ .config/yay/init.lua
@@ -0,0 +1,6 @@
+-- yay settings. Overlays config.json; CLI flags still win over both.
+
+-- Replaces the old `--devel --timeupdate`; --timeupdate was removed in yay 13.
+yay.opt.devel = true
+yay.opt.answer_diff = "All"
+yay.opt.remove_make = "yes"
